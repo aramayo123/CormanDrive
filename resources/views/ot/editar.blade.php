67ad2c297f06859193e5ab79496b57ab2df58dda
aramayo123 <aramayo420@gmail.com>
2024-05-01 16:14:02 -0300

    Agregada advertencia de que demora mucho en edit

--- resources/views/ot/editar.blade.php
+++ resources/views/ot/editar.blade.php
@@ -36,6 +36,17 @@
             </div>
         @endif
        </div>
+       <div class="mx-5 my-2">
+            <div id="alert-3" class="flex max-w-full sm:max-w-xl mx-auto items-center p-4 mb-4 text-yellow-400 rounded-lg bg-gray-800" role="alert">
+                <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+                <span class="sr-only">Info</span>
+                <div class="ml-3 text-sm font-medium">
+                    <p>SI CAMBIAS EL REMEDIT Y/O PASAS A COMBUSTIBLE, PUEDE DEMORAR UN POCO MAS.</p>
+                </div>
+            </div>
+        </div>
        <div class="mx-5 my-6">
             <form method="post" action="{{ url('ot/editar/'.$remedit->id) }}" class="max-w-xl mx-auto my-6 rounded-lg bg-gray-800 p-6" >
                 @csrf
